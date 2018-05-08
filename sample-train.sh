f135004e85649b0e5f2c5b1e39c9f04412189297
lichenliang-whu <lichenliang86@gmail.com>
2018-05-08 11:46:01 +0800

    Update sample-train.sh

--- sample-train.sh
+++ sample-train.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES='0' python model.py sample.config --train --train_file 20ng_train.txt --validation_file 20ng_val.txt --checkpoint_dir output/
+CUDA_VISIBLE_DEVICES='0' python model.py sample.config --train --train_file dataset/20ng_train.txt --validation_file dataset/20ng_val.txt --checkpoint_dir output/
